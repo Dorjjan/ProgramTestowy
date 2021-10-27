17f9e6cab6de6566d2479586c15238fa2302bd44
Dorjjan <dareklewandowski8@gmail.com>
2021-10-27 14:27:12 +0200

    working on a bonus point

--- app.php
+++ app.php
@@ -1,35 +1,51 @@
 <?php
 class Game{
-    private $pins;
+    private $pins = 10;
     private $input;
     private $score;
+    private $spare;
+    private $strike;
     public function roll(){
-        $this->pins = 10;
-        for($i=0;$i<2;$i++){
-             if($this->pins<=0)
-             {
-                 echo "0 pins left";
-                 break;
+        if($this->pins<=0){
+            echo "0 Pins left. Next round". "\n";
+            $this->pins = 10;
+        }
+        else{
+            for($i=0;$i<2;$i++){
+                 if($this->pins<=0 && $i>=1) {
+                     echo "STRIKE!". "\n";
+                     $this->input = readline("");
+                     settype($this->input, 'integer');
+                     $this->pins = $this->pins - $this->input;
+                     $this->strike = $this->strike + $this->input + $this->input;
+                     break;
+                }  
+                 elseif ($this->pins<=0 && $i=0){
+                     echo "SPARE!". "\n";
+                     $this->input = readline("");
+                     settype($this->input, 'integer');
+                     $this->pins = $this->pins - $this->input;
+                     $this->spare = $this->spare + $this->input;
              }
-             else{
-            echo "Roll". "\n";
-            $this->input = readline("");
-            settype($this->input, 'integer');
-            $this->pins = $this->pins - $this->input;
-           
+                 else{
+                     echo "Roll". "\n";
+                     $this->input = readline("");
+                     settype($this->input, 'integer');
+                     $this->pins = $this->pins - $this->input;
+                     $this->score = $this->score + $this->input;
              }
-         }
-         
-        
+            }
+        }
     }
-    public function getScore(){
-     $this->score = $this->score + $this->input;
-     echo $this->input;
-     echo "You're score is $this->score". "\n";
-    }
+         public function getScore(){
+        $this->score = $this->score + $this->strike + $this->spare;
+        echo "You're score is $this->score". "\n";
+     }
     public function finalResult(){
 
     }
+
+
 }
 $game = new Game();
 echo "Let's play a game!". "\n";
